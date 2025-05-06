7a133039084526166ddeeb52c3c489b7b342dbcb
MiloHakala <Milohakal4@gmail.com>
2025-05-06 15:29:34 +0300

    15,30

---
+++ Assets/Scripts/FryingPan.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class FryingPan : MonoBehaviour
+{
+    public bool isFrying = false;
+    public AudioSource fryingSound; // Assign in Inspector
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (animator == null)
+        {
+            Debug.LogError("FryingPan: No Animator component found!");
+        }
+    }
+
+    void Update()
+    {
+        if (animator != null)
+        {
+            animator.SetBool("IsFrying", isFrying);
+        }
+        if (isFrying && !fryingSound.isPlaying)
+        {
+            fryingSound.Play();
+        }
+        else if (!isFrying && fryingSound.isPlaying)
+        {
+            fryingSound.Stop();
+        }
+    }
+}
